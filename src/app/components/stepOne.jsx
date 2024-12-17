3eb15969a209c31a74de9f9dd51d3d50261cb2a3
azaaa-9 <az95342030@gmail.com>
2024-12-17 23:38:01 +0800

    continue 2/3

--- src/app/components/stepOne.jsx
+++ src/app/components/stepOne.jsx
@@ -1,54 +1,58 @@
 
-import Image from "next/image";
 export function StepOne({ setSteps }) {
-    return (
-        <div className="flex items-center justify-center min-h-screen bg-gray-100">
+  return (
+ <div className="flex items-center justify-center min-h-screen bg-gray-100 space-x-8">
+
+ <div className="w-full max-w-md p-8 bg-white rounded-lg shadow-lg  h-[655px]">
+
+  <img src="/main.png" />
+  
+  <h1 className="text-2xl font-bold text-gray-800 mb-2">
+          Join Us! <span role="img" aria-label="cool">😎</span>
+ </h1>
+
+ <p className="text-gray-600 mb-6">
+          Please provide all current information accurately.
+ </p>
+
+ <form className="space-y-4 text-black">
+ <div>
+ <label className="block text-gray-700 font-medium mb-1">First name <span className="text-red-500">*</span>
+ </label>
+ <input
+              type="text"
+              placeholder="Placeholder"
+              className="w-full px-4 py-2 border rounded-md focus:ring-2 focus:ring-blue-400 outline-none"
+            />
+ </div>
 
 
-        <div className="bg-white p-8 rounded-lg shadow-lg max-w-md  text-black w-[480px] h-[655px]">
-          <img className="w-[60px] h-[60px]" src="/main.png" />
-          <h1 className="text-2xl font-bold mb-4 text-center "> Join Us! 😎</h1>
-          <p className="text-gray-600 mb-6 text-center">
-            Step 1 of 3 - Please provide all current information accurately.
-          </p>
-
-
-                <div className="text-black">
-                    <p >First Name *</p>
-                    <input 
-                    className=" border border-solid border-[#CBD5E1] hover:border-[#0CA5E9] w-full rounded h-[44px]"
-                    placeholder="First Name"></input>
-                    <p>Last Name *</p>
-                    <input 
-                    className=" border border-solid border-[#CBD5E1] hover:border-[#0CA5E9] w-full rounded h-[44px]"
-                    placeholder="Last Name"></input>
-                    <p>Username *</p>
-                    <input 
-                    className=" border border-solid border-[#CBD5E1] hover:border-[#0CA5E9] w-full rounded h-[44px]"
-                    placeholder="Username"></input>
-                     </div>
-
-
-                     <div >
-                     <button 
-                     className="border w-full p-2 h-[44px] "
+ <div>
+ <label className="block text-gray-700 font-medium mb-1">Last name <span className="text-red-500">*</span>
+ </label>
+ <input
+              type="text"
+              placeholder="Placeholder"
+              className="w-full px-4 py-2 border rounded-md focus:ring-2 focus:ring-blue-400 outline-none"
+            />
+ </div>
+ <div>
+ <label className="block text-gray-700 font-medium mb-1">Username <span className="text-red-500">*</span>
+ </label>
+ <input
+              type="text"
+              placeholder="Placeholder"
+              className="w-full px-4 py-2 border rounded-md focus:ring-2 focus:ring-blue-400 outline-none"
+            />
+ </div>
+ <button 
+                     className="border w-full p-2 h-[44px] text-black "
                      onClick={() => {
                         setSteps(2)}} >Continue 1/3</button>
-                        </div>
+ </form>
 
 
-                     </div>
-        </div>
-    
-    )
-}
-
-
-
-
-
-
-
-
-
- 
+ </div>
+ </div>
+  );
+ }
